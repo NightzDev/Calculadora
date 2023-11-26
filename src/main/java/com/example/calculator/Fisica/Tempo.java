2ea9a92b61ed81e82b9160a288c1642239996bbc
Matheus Costa <matheuscta@gmail.com>
2023-11-25 21:43:29 -0300

    atualizei fisica

--- src/main/java/com/example/calculator/Fisica/Tempo.java
+++ src/main/java/com/example/calculator/Fisica/Tempo.java
@@ -1,5 +1,17 @@
 package com.example.calculator.Fisica;
 
 public class Tempo {
-
+	public double Vmedia(double ds, double vm) {
+		
+		double dt;
+	/*   
+	      dt=ds/vm 
+	      vm= velocidade media
+	      ds=deslocamento
+	      dt=intervalo de tempo
+	      */
+		dt= ds / vm;
+		return dt;
+	
+	}
 }
